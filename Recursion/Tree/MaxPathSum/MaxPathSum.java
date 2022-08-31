47703bf768072d085245b69693267d85919fb24d
chuanlingShi <94197901+chuanlingShi@users.noreply.github.com>
2022-08-31 15:17:27 -0400

    added TreeNode class and implemented Get Max Sum path of BT (leaf node)

---
+++ Recursion/Tree/MaxPathSum/MaxPathSum.java
@@ -0,0 +1,41 @@
+package Recursion.Tree.MaxPathSum;
+import test.TreeNode;
+/***
+ * Given a binary tree in which each node contains an int number.
+ *
+ * Find the maximum possible sum from any leaf node to another leaf node.
+ *
+ * The maximum sum path may or may not go through root.
+ *
+ * Expected time complexity is O(n).
+ */
+
+public class MaxPathSum {
+    public int maxPathSum(TreeNode root) {
+        int[] maxSum = new int[1];
+        maxSum[0] = Integer.MIN_VALUE;
+        maxGain(root, maxSum);
+        return maxSum[0];
+    }
+
+    private int maxGain(TreeNode node, int[] maxSum) {
+        if(node == null){
+            return 0;
+        }
+
+        if(node.left == null && node.right == null) {
+            return node.key;
+        }
+
+        int leftGain = maxGain(node.left, maxSum);
+        int rightGain = maxGain(node.right, maxSum);
+
+        if(node.left != null && node.right != null) {
+            int newPathGain = node.key + leftGain + rightGain;
+            maxSum[0] = Math.max(maxSum[0], newPathGain);
+            return node.key + Math.max(leftGain, rightGain);
+        }
+
+        return node.left == null ? rightGain + node.key : leftGain + node.key;
+    }
+}
---
+++ test/TreeNode.java
@@ -0,0 +1,12 @@
+package test;
+
+public class TreeNode {
+    public int key;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int value){
+        this.key = value;
+    }
+
+}
