b78a4dffaca156f71386980a0b168e4902f116d6
chuanlingShi <94197901+chuanlingShi@users.noreply.github.com>
2022-09-29 13:54:09 -0400

    add Merge K Sorted Array

---
+++ CrossTrainingIII/MergeKSortedArray.java
@@ -0,0 +1,61 @@
+package CrossTrainingIII;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * Merge K sorted array into one big sorted array in ascending order.
+ *
+ * Assumptions
+ *
+ * The input arrayOfArrays is not null, none of the arrays is null either.
+ */
+public class MergeKSortedArray {
+    public int[] merge(int[][] arrayOfArrays) {
+        PriorityQueue<Entry> minHeap = new PriorityQueue<Entry>(11, new MyComparator());
+
+        int length = 0;
+        for (int i = 0; i < arrayOfArrays.length; i++) {
+            int[] array = arrayOfArrays[i];
+            length += array.length;
+            if (array.length != 0) {
+                minHeap.offer(new Entry(i, 0, array[0]));
+            }
+        }
+        int[] result = new int[length];
+        int cur = 0;
+        while(!minHeap.isEmpty()) {
+            Entry tmp = minHeap.poll();
+            result[cur++] = tmp.value;
+            if (tmp.y + 1 < arrayOfArrays[tmp.x].length) {
+                tmp.y++;
+                tmp.value = arrayOfArrays[tmp.x][tmp.y];
+                minHeap.offer(tmp);
+            }
+        }
+        return result;
+    }
+
+    static class MyComparator implements Comparator<Entry> {
+        @Override
+        public int compare(Entry e1, Entry e2) {
+            if (e1.value == e2.value) {
+                return 0;
+            }
+            return e1.value < e2.value ? -1 : 1;
+        }
+    }
+
+    static class Entry {
+        int x;
+        int y;
+        int value;
+
+        Entry(int x, int y, int value) {
+            this.x = x;
+            this.y = y;
+            this.value = value;
+        }
+    }
+}
