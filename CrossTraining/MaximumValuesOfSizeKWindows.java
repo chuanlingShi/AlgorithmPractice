1956b57a17465392b188bb7b87226bc7dbd1e781
chuanlingShi <94197901+chuanlingShi@users.noreply.github.com>
2022-10-03 12:26:14 -0400

    Maximum Values Of Size K Sliding Windows

---
+++ CrossTraining/MaximumValuesOfSizeKWindows.java
@@ -0,0 +1,26 @@
+package CrossTraining;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MaximumValuesOfSizeKWindows {
+    public List<Integer> maxWindows(int[] array, int k) {
+        List<Integer> max = new ArrayList<>();
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 0; i < array.length; i++) {
+            while (!deque.isEmpty() && array[deque.peekLast()] <= array[i]) {
+                deque.pollLast();
+            }
+            if (!deque.isEmpty() && deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+            deque.offerLast(i);
+            if (i >= k - 1) {
+                max.add(array[deque.peekFirst()]);
+            }
+        }
+        return max;
+    }
+}
