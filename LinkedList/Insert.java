5a9e3d5d9ca4eeb0ed763a9c9cf457c61b31c820
chuanlingShi <94197901+chuanlingShi@users.noreply.github.com>
2023-01-08 20:15:11 -0500

    insert a node in a sorted list

---
+++ LinkedList/Insert.java
@@ -0,0 +1,22 @@
+package LinkedList;
+
+public class Insert {
+    //tc: O(n)
+    //sc: O(1)
+    public ListNode insert(ListNode head, int value) {
+        // Write your solution here
+        ListNode target = new ListNode(value);
+        if (head == null || head.next == null) {
+            target.next = head;
+            return target;
+        }
+        ListNode cur = head;
+        while (cur.next != null && cur.value < target.value) {
+            cur = cur.next;
+        }
+        ListNode next = cur.next;
+        cur.next = target;
+        target.next = next;
+        return head;
+    }
+}
