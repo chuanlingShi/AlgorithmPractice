f7953d0bffff49ee42e6c15eba5bb0df545603f4
chuanlingShi <94197901+chuanlingShi@users.noreply.github.com>
2023-01-09 19:49:14 -0500

    check if a linked list has a cycle

---
+++ LinkedList/HasCycle.java
@@ -0,0 +1,17 @@
+package LinkedList;
+
+public class HasCycle {
+    //tc: O(n)
+    //sc: O(1)
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow.value == fast.value) return true;
+        }
+        return false;
+    }
+}
