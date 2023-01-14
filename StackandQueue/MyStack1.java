01d7d42fc09d961b167c1c0f217b23648ea9dd12
chuanlingShi <94197901+chuanlingShi@users.noreply.github.com>
2023-01-13 23:03:47 -0500

    implement stack with linked list

---
+++ StackandQueue/MyStack1.java
@@ -0,0 +1,54 @@
+package StackandQueue;
+
+public class MyStack1 {
+    private ListNode top;
+    private int size;
+
+    public MyStack1(int top) {
+        this.top = new ListNode(top);
+        size = 1;
+    }
+
+    public MyStack1() {
+        top = null;
+        size = 0;
+    }
+
+    public Integer pop() {
+        if (isEmpty()) {
+            return null;
+        }
+        ListNode oldTop = top;
+        ListNode newTop = top.next;
+        oldTop.next = null;
+        top = newTop;
+        size--;
+        return oldTop.value;
+
+    }
+
+    public boolean push(int top) {
+        ListNode newTop = new ListNode(top);
+        newTop.next = this.top;
+        this.top = newTop;
+        size++;
+        return true;
+    }
+
+    public Integer peek() {
+        if (isEmpty()) {
+            return null;
+        }
+        return top.value;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+
+}
---
+++ StackandQueue/SortWith2Stacks.java
@@ -0,0 +1,11 @@
+package StackandQueue;
+
+import java.util.LinkedList;
+
+public class SortWith2Stacks {
+    public void sort(LinkedList<Integer> s1) {
+        LinkedList<Integer> s2 = new LinkedList<Integer>();
+
+
+    }
+}
