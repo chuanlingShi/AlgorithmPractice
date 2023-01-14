896b7cb74899486080b4940dd1879bd0f854c020
chuanlingShi <94197901+chuanlingShi@users.noreply.github.com>
2023-01-14 16:15:17 -0500

    sort with 2 stacks

--- StackandQueue/SortWith2Stacks.java
+++ StackandQueue/SortWith2Stacks.java
@@ -1,11 +1,45 @@
 package StackandQueue;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class SortWith2Stacks {
     public void sort(LinkedList<Integer> s1) {
-        LinkedList<Integer> s2 = new LinkedList<Integer>();
-
-
+        if (s1.size() == 1) return;
+        Deque<Integer> s2 = new ArrayDeque<>();
+        sort(s1, s2);
     }
+
+    private void sort(Deque s1, Deque s2) {
+        while (!s1.isEmpty()) {
+            int globalMin = Integer.MAX_VALUE;
+            int countMin = 0;
+            //数
+            while (!s1.isEmpty()) {
+                int cur = (int) s1.pollFirst();
+                if (cur < globalMin) {
+                    globalMin = cur;
+                    countMin = 1;
+                } else if (cur == globalMin) {
+                    countMin++;
+                }
+                s2.offerFirst(cur);
+            }
+            //分拣
+            while (!s2.isEmpty() && (int) s2.peekFirst() >= globalMin) {
+                int cur = (int) s2.pollFirst();
+                if (cur > globalMin) {
+                    s1.offerFirst(cur);
+                }
+            }
+            for (int i = 0; i < countMin; i++) {
+                s2.offerFirst(globalMin);
+            }
+        }
+        while (!s2.isEmpty()) {
+            s1.offerFirst(s2.pollFirst());
+        }
+    }
+
 }
