74212983622392952914f04d2fb9815a5ccc518c
chuanlingShi <94197901+chuanlingShi@users.noreply.github.com>
2023-01-25 16:52:51 -0500

    heap offer operation

---
+++ heap/HeapOffer.java
@@ -0,0 +1,24 @@
+package heap;
+
+public class HeapOffer {
+    public int[] offerHeap(int[] array, int ele) {
+        int index = array.length - 1;
+        array[index] = ele;
+        while (index > 0) {
+            int parentIndex = (index - 1) / 2;
+            if (array[parentIndex] > array[index]) {
+                swap(array,parentIndex, index);
+                index = parentIndex;
+            }else {
+                break;
+            }
+        }
+        return array;
+    }
+
+    private void swap(int[] array, int left, int right) {
+        int temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
+    }
+}
